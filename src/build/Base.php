81705c051ea53b6b1b2fc7957fd5fb3f7c8ec76e
houdunwang <2300071698@qq.com>
2017-03-15 14:20:25 +0800

    no message

--- src/build/Base.php
+++ src/build/Base.php
@@ -26,8 +26,6 @@ class Base {
 	protected $desc = [ 'pre' => '上一页', 'next' => '下一页', 'first' => '首页', 'end' => '尾页', 'unit' => '条' ];
 	//前页页码显示样式
 	protected $show;
-	//配置
-	protected $config;
 
 	/**
 	 * 显示条数
