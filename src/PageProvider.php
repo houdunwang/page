4606cca599e94b9eeb576e80c4d4ebebbfeb193b
houdunwang <2300071698@qq.com>
2017-06-18 05:04:56 +0800

    no message

--- src/PageProvider.php
+++ src/PageProvider.php
@@ -7,21 +7,25 @@
  * |    WeChat: aihoudun
  * | Copyright (c) 2012-2019, www.houdunwang.com. All Rights Reserved.
  * '-------------------------------------------------------------------*/
+
 namespace houdunwang\page;
 
 use houdunwang\framework\build\Provider;
 
-class PageProvider extends Provider {
+class PageProvider extends Provider
+{
 
-	//延迟加载
-	public $defer = true;
+    //延迟加载
+    public $defer = true;
 
-	public function boot() {
-	}
+    public function boot()
+    {
+    }
 
-	public function register() {
-		$this->app->single( 'Page', function ( $app ) {
-			return Page::single( $app );
-		} );
-	}
+    public function register()
+    {
+        $this->app->single('Page', function ($app) {
+            return new Page($app);
+        });
+    }
 }
